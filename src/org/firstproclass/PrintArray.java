f2935942db7023c0c27de2384607456f607fc217
unknown <rmprasanna.rm@gmail.com>
2019-08-17 00:50:04 +0530

    address the review comment

--- src/org/firstproclass/PrintArray.java
+++ src/org/firstproclass/PrintArray.java
@@ -4,11 +4,11 @@ public class PrintArray {
 public static void main(String[] args) {
 	
 	
-	int[] a = {2,3,4,5,6};
+	int[] array = {2,3,4,5,6};
 	
-	for (int i = 0; i < a.length; i++) {
+	for (int i = 0; i < array.length; i++) {
 		
-		System.out.println(i);
+		System.out.println(array[i]);
 		
 	}
 	
