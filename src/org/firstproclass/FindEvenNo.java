6c1288ab97fdc1bc2812efd1c51ef4832ea53ca7
unknown <rmprasanna.rm@gmail.com>
2019-08-16 16:57:58 +0530

    vikram added new code

---
+++ src/org/firstproclass/FindEvenNo.java
@@ -0,0 +1,18 @@
+package org.firstproclass;
+
+public class FindEvenNo {
+	public static void main(String[] args) {
+		
+		int n =9;
+		
+		if(n%2==0)
+		{
+			System.out.println("Number is even");
+		}else
+		{
+			System.out.println("Number is odd");
+		}
+		
+	}
+
+}
